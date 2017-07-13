2be652bc66a6246eaf729eb5fb0762065687c18e
蒋亚林 <18725944157@163.com>
2017-07-14 00:13:18 +0800

    xx

--- src/js/components/Panel/index.js
+++ src/js/components/Panel/index.js
@@ -10,7 +10,7 @@ const Panel = () => {
       </div>
       <PanelMenuClass title="星标项目" />
       <div>
-        <PanelMenu text="一方通行的学习项目1" option="false" />
+        <PanelMenu text="一方通行的学习项目" option="false" />
         <PanelMenu text="一方通行的学习项目2" option="false" />
       </div>
       <PanelMenuClass title="星标项目" />
